eea767907833562d67b86182cd6a7497613c7aef
Danil <112797010+JordennJd@users.noreply.github.com>
2026-04-15 11:26:51 +0300

    feat: introduce NanosystemSeriesGridifyMapper for enhanced query mapping in NanoSystemSeriesStorage

--- SaxsSpot.NanoSystemService.Storage/Storages/NanoSystemSeriesStorage.cs
+++ SaxsSpot.NanoSystemService.Storage/Storages/NanoSystemSeriesStorage.cs
@@ -10,9 +10,11 @@ namespace SaxsSpot.NanoSystemService.Storage;
 public class NanoSystemSeriesStorage(NanoSystemSeriesDbContext dbContext)
     : GenericStorage<NanosystemSeries>(dbContext), INanoSystemSeriesStorage
 {
+    private static readonly NanosystemSeriesGridifyMapper GridifyMapper = new();
+
     public Task<Paging<NanosystemSeries>> Gridify(GridifyQuery query)
     {
-        return dbContext.Entities.GridifyAsync(query);
+        return dbContext.Entities.GridifyAsync(query, GridifyMapper);
     }
 
     public async Task DeleteAsync(NanosystemSeries entity)
---
+++ SaxsSpot.NanoSystemService.Storage/Storages/NanosystemSeriesGridifyMapper.cs
@@ -0,0 +1,33 @@
+using Gridify;
+using SaxsSpot.NanoSystemService.Domain;
+
+namespace SaxsSpot.NanoSystemService.Storage;
+
+public sealed class NanosystemSeriesGridifyMapper : GridifyMapper<NanosystemSeries>
+{
+    public NanosystemSeriesGridifyMapper()
+    {
+        AddMap("Id", x => x.Id);
+        AddMap("ParticleKind", x => x.ParticleKind);
+        AddMap("ParticleCountFrom", x => x.ParticleCountFrom);
+        AddMap("ParticleCountTo", x => x.ParticleCountTo);
+        AddMap("GlobalSizeFrom", x => x.GlobalSizeFrom);
+        AddMap("GlobalSizeTo", x => x.GlobalSizeTo);
+        AddMap("NumericalConcentrationFrom", x => x.NumericalConcentrationFrom);
+        AddMap("NumericalConcentrationTo", x => x.NumericalConcentrationTo);
+        AddMap("ExcessFrom", x => x.ExcessFrom);
+        AddMap("ExcessTo", x => x.ExcessTo);
+        AddMap("MaxParticleSizeFrom", x => x.MaxParticleSizeFrom);
+        AddMap("MaxParticleSizeTo", x => x.MaxParticleSizeTo);
+        AddMap("MinParticleSizeFrom", x => x.MinParticleSizeFrom);
+        AddMap("MinParticleSizeTo", x => x.MinParticleSizeTo);
+        AddMap("KFrom", x => x.KFrom);
+        AddMap("KTo", x => x.KTo);
+        AddMap("ThetaFrom", x => x.ThetaFrom);
+        AddMap("ThetaTo", x => x.ThetaTo);
+        AddMap("Comment", x => x.Comment);
+        AddMap("CreatedAt", x => x.CreatedAt);
+        AddMap("createdAt", x => x.CreatedAt);
+        AddMap("created_at", x => x.CreatedAt);
+    }
+}
